323e92114a82c7258e6c229d367c7a3f9a92ff85
mariabelenamat <belen.amat29@gmail.com>
2019-07-12 15:22:34 -0300

    agrego archivo de schema y resolvers

--- graphQL-server/src/index.js
+++ graphQL-server/src/index.js
@@ -2,14 +2,13 @@ import express from "express"
 import graphqlHTTP from "express-graphql"
 const app = express();
 
-const schema  = {};
-
 app.get('/', (req, res) => {
     res.json({message: 'tu vieja'})
 })
 app.use('/graphql', graphqlHTTP({
     //Utiliza graphqlHTTP oara procesar esa ruta
     graphiql: true,
+    //UI para request (vendria a ser como postman)
     schema: schema
 }))
 app.listen(3000, () => console.log('server running on port 3000'))
---
+++ graphQL-server/src/schema.js
@@ -0,0 +1,7 @@
+import {} from './resolvers.js'
+//para que sirve el schema? para saber cuales son los datos que podemos pedir, y saber (aproximadamente) como son los datos que vamos a obtener
+//GraphQL Schema language. Es similar al lenguaje de consulta PERO NO ES EL MISMO. Permite abstreaernos del lenguaje del backend
+//The most basic components of a GraphQL Schema are object types, which just represent a kind of object you can fetch from your service, and what fields it has.
+
+const typesDefinitions = '';
+//ACA SE DEFINE QUE ES LO QUE SE PUEDE CONSULTAR
